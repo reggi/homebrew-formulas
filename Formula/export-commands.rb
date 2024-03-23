3bec5747bedba8b8d1f0a50e5bba5d642d13051f
Thomas Reggi <thomas@reggi.com>
2024-03-23 03:04:37 -0400

    1.0.2

--- Formula/export-commands.rb
+++ Formula/export-commands.rb
@@ -1,14 +1,14 @@
 class ExportCommands < Formula
   desc "Converts export expressions to commands"
   homepage "https://github.com/reggi/export-commands"
-  url "https://github.com/reggi/export-commands/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "fecc9b9b431b81581939f1d0cf507081e903af3ce4dd793770da2d0702d31ca1"
+  url "https://github.com/reggi/export-commands/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "7be8b59c5919e0f0c321177c389b55cc32b332784e1ec5aba21767442573e4dd"
   license "MIT"
 
   depends_on "deno"
 
   def install
-    bin.install "export-command.sh" => "export-commands"
+    bin.install "export-commands.sh" => "export-commands"
   end
 
   test do
